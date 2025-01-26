dd3c1cb772a847dafcd106862887e51a249cead7
Tristan Andrus <tristan@tristanandrus.com>
2025-01-26 10:52:46 -0700

    Add buildanddeploy.sh
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

---
+++ rustblock/buildanddeploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# File:        buildanddeploy.sh
+# Author:      Tristan Andrus
+# Description: Builds for my router and deploys it
+################################################################################
+
+set -o errexit   # Abort on nonzero exitstatus
+set -o nounset   # Abort on unbound variable
+set -o pipefail  # Don't hide errors within pipes
+
+trap "echo 'An error occurred! Quitting mid-script!'" ERR
+
+# Uncomment to debug
+# set -x
+
+################################################################################
+
+cross build --target armv7-unknown-linux-musleabi --release
+
+scp -O ./target/armv7-unknown-linux-musleabi/release/rustblock router:/jffs
